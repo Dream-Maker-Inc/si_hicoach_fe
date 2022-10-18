657a9ad349da994759334c96f6a80c475f5f0481
wkdgus1164 <wkdgus1164@naver.com>
2022-10-19 00:09:44 +0900

    refactor: SafeArea 적용

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -30,115 +30,117 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(
-              left: defaultPadding,
-              right: defaultPadding,
-              top: 100,
-              bottom: defaultPadding,
+      body: SafeArea(
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(
+                left: defaultPadding,
+                right: defaultPadding,
+                top: 100,
+                bottom: defaultPadding,
+              ),
+              child: Text(
+                '퍼스널 트레이닝을 더 가치있게',
+                style: Theme.of(context).textTheme.overline,
+                textAlign: TextAlign.center,
+              ),
             ),
-            child: Text(
-              '퍼스널 트레이닝을 더 가치있게',
-              style: Theme.of(context).textTheme.overline,
-              textAlign: TextAlign.center,
-            ),
-          ),
-          const Image(image: AssetImage('assets/logo.png')),
-          Container(
-            padding: const EdgeInsets.only(
-              top: 90,
-              left: defaultPadding,
-              right: defaultPadding,
-            ),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: '아이디 (이메일)을 입력하세요.',
-                      labelText: '아이디 (이메일)',
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                    controller: _emailController,
-                    validator: (value) {
-                      if (value!.trim().isEmpty) {
-                        return '아이디(이메일)을 입력해 주세요.';
-                      }
-                      return null;
-                    },
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top: defaultPadding),
-                    width: double.infinity,
-                    child: TextFormField(
+            const Image(image: AssetImage('assets/logo.png')),
+            Container(
+              padding: const EdgeInsets.only(
+                top: 90,
+                left: defaultPadding,
+                right: defaultPadding,
+              ),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
                       decoration: const InputDecoration(
                         border: OutlineInputBorder(),
-                        hintText: '비밀번호를 입력하세요.',
-                        labelText: '비밀번호',
+                        hintText: '아이디 (이메일)을 입력하세요.',
+                        labelText: '아이디 (이메일)',
                       ),
-                      obscureText: true,
-                      controller: _passwordController,
+                      keyboardType: TextInputType.emailAddress,
+                      controller: _emailController,
                       validator: (value) {
                         if (value!.trim().isEmpty) {
-                          return '비밀번호를 입력해 주세요.';
+                          return '아이디(이메일)을 입력해 주세요.';
                         }
                         return null;
                       },
                     ),
-                  ),
-                  Container(
-                    alignment: Alignment.topRight,
-                    child: const TextButton(
-                      onPressed: _handleTextButtonPressed,
-                      child: Text(
-                        '회원정보 찾기',
-                        style: TextStyle(
-                          color: Colors.grey,
-                          decoration: TextDecoration.underline,
+                    Container(
+                      margin: const EdgeInsets.only(top: defaultPadding),
+                      width: double.infinity,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: '비밀번호를 입력하세요.',
+                          labelText: '비밀번호',
+                        ),
+                        obscureText: true,
+                        controller: _passwordController,
+                        validator: (value) {
+                          if (value!.trim().isEmpty) {
+                            return '비밀번호를 입력해 주세요.';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.topRight,
+                      child: const TextButton(
+                        onPressed: _handleTextButtonPressed,
+                        child: Text(
+                          '회원정보 찾기',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            decoration: TextDecoration.underline,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top: defaultPadding),
-                    width: double.infinity,
-                    child: CustomElevatedButton(
-                      onPressed: _handleButtonPressed,
-                      text: '로그인',
+                    Container(
+                      margin: const EdgeInsets.only(top: defaultPadding),
+                      width: double.infinity,
+                      child: CustomElevatedButton(
+                        onPressed: _handleButtonPressed,
+                        text: '로그인',
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 90),
-            child: Text(
-              '아직 회원이 아니세요?',
-              style: Theme.of(context).textTheme.overline,
-              textAlign: TextAlign.center,
+            Container(
+              margin: const EdgeInsets.only(top: 90),
+              child: Text(
+                '아직 회원이 아니세요?',
+                style: Theme.of(context).textTheme.overline,
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(
-              top: smallPadding,
-              left: defaultPadding,
-              right: defaultPadding,
-              bottom: defaultPadding,
+            Container(
+              margin: const EdgeInsets.only(
+                top: smallPadding,
+                left: defaultPadding,
+                right: defaultPadding,
+                bottom: defaultPadding,
+              ),
+              width: double.infinity,
+              child: CustomOutlinedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/sign_up');
+                },
+                text: '30초 회원가입',
+              ),
             ),
-            width: double.infinity,
-            child: CustomOutlinedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/sign_up');
-              },
-              text: '30초 회원가입',
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/pages/sign_up/sign_up_agreement.dart
+++ lib/pages/sign_up/sign_up_agreement.dart
@@ -26,97 +26,100 @@ class _SignUpAgreementPageState extends State<SignUpAgreementPage> {
           },
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Column(
-            children: [
-              LinearProgressIndicator(
-                value: 0.25,
-                color: Colors.indigo,
-                backgroundColor: Colors.indigo.withOpacity(0.5),
-              ),
-              Container(
-                margin: const EdgeInsets.all(defaultPadding),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Text(
-                      '약관 동의',
-                      style: Theme.of(context).textTheme.headline1,
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      '앱 사용을 위해 필수 이용 약관에 동의해 주세요.',
-                      style: Theme.of(context).textTheme.caption,
-                    )
-                  ],
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              children: [
+                LinearProgressIndicator(
+                  value: 0.25,
+                  color: Colors.indigo,
+                  backgroundColor: Colors.indigo.withOpacity(0.5),
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(
-                  top: defaultPadding,
-                  left: defaultPadding,
-                  right: defaultPadding,
+                Container(
+                  margin: const EdgeInsets.all(defaultPadding),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Text(
+                        '약관 동의',
+                        style: Theme.of(context).textTheme.headline1,
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        '앱 사용을 위해 필수 이용 약관에 동의해 주세요.',
+                        style: Theme.of(context).textTheme.caption,
+                      )
+                    ],
+                  ),
                 ),
-                child: ExpansionPanelList(
-                  expansionCallback: (int index, bool isExpanded) {
-                    setState(() {
-                      _isCarouselExpanded = !_isCarouselExpanded;
-                    });
-                  },
-                  children: [
-                    ExpansionPanel(
-                      headerBuilder: (BuildContext context, bool isExpanded) {
-                        return ListTile(
-                          title: Text(
-                            '모든 약관에 동의합니다.',
-                            style: TextStyle(
-                              color:
-                                  _isAllChecked ? Colors.indigo : Colors.black,
+                Container(
+                  margin: const EdgeInsets.only(
+                    top: defaultPadding,
+                    left: defaultPadding,
+                    right: defaultPadding,
+                  ),
+                  child: ExpansionPanelList(
+                    expansionCallback: (int index, bool isExpanded) {
+                      setState(() {
+                        _isCarouselExpanded = !_isCarouselExpanded;
+                      });
+                    },
+                    children: [
+                      ExpansionPanel(
+                        headerBuilder: (BuildContext context, bool isExpanded) {
+                          return ListTile(
+                            title: Text(
+                              '모든 약관에 동의합니다.',
+                              style: TextStyle(
+                                color: _isAllChecked
+                                    ? Colors.indigo
+                                    : Colors.black,
+                              ),
                             ),
-                          ),
-                          leading: Checkbox(
-                            value: _isAllChecked,
-                            onChanged: (value) {
+                            leading: Checkbox(
+                              value: _isAllChecked,
+                              onChanged: (value) {
+                                setState(() {
+                                  _isAllChecked = value!;
+                                });
+                              },
+                              activeColor: Colors.indigo,
+                            ),
+                            onTap: () {
                               setState(() {
-                                _isAllChecked = value!;
+                                _isAllChecked = !_isAllChecked;
                               });
                             },
-                            activeColor: Colors.indigo,
-                          ),
-                          onTap: () {
-                            setState(() {
-                              _isAllChecked = !_isAllChecked;
-                            });
-                          },
-                        );
-                      },
-                      body: Column(
-                        children: const <Widget>[
-                          AgreementListItem(text: '[필수] 서비스 이용약관'),
-                          AgreementListItem(text: '[필수] 개인정보 처라방침'),
-                        ],
-                      ),
-                      isExpanded: _isCarouselExpanded,
-                    )
-                  ].toList(),
+                          );
+                        },
+                        body: Column(
+                          children: const <Widget>[
+                            AgreementListItem(text: '[필수] 서비스 이용약관'),
+                            AgreementListItem(text: '[필수] 개인정보 처라방침'),
+                          ],
+                        ),
+                        isExpanded: _isCarouselExpanded,
+                      )
+                    ].toList(),
+                  ),
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: double.infinity,
+                margin: const EdgeInsets.all(defaultPadding),
+                child: CustomElevatedButton(
+                  onPressed: () => _handleSubmitButtonPressed(context),
+                  text: '다음',
                 ),
               ),
-            ],
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              width: double.infinity,
-              margin: const EdgeInsets.all(defaultPadding),
-              child: CustomElevatedButton(
-                onPressed: () => _handleSubmitButtonPressed(context),
-                text: '다음',
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
