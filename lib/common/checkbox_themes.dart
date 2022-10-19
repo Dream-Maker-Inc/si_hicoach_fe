f4a00ca0f90218f0d20dd939b7651de6f73c9912
andy <wkdgus1164@naver.com>
2022-10-19 15:05:37 +0900

    refactor: checkbox_themes.dart 테마 컬러 설정

--- lib/common/checkbox_themes.dart
+++ lib/common/checkbox_themes.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
-var checkboxThemes = CheckboxThemeData();
+var checkboxThemes = CheckboxThemeData(
+  // 체크하기 전 체크박스 테두리
+  fillColor: MaterialStateProperty.all(Colors.grey),
+);
