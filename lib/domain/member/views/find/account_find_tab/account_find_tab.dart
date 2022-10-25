971dd0a2d541a68dfa2f7641a549db8f75327723
andy <wkdgus1164@naver.com>
2022-10-25 18:29:41 +0900

    refactor: 코드 warning 정리

--- lib/domain/member/views/find/account_find_tab/account_find_tab.dart
+++ lib/domain/member/views/find/account_find_tab/account_find_tab.dart
@@ -30,50 +30,43 @@ class TabSection extends StatelessWidget {
           backgroundColor: Colors.white,
           centerTitle: false,
           titleSpacing: 0.0,
-          title: Text('회원정보 찾기',
-              style: TextStyle(
-                color: Colors.black,
-              )),
-          leading: IconButton(
-              icon: Icon(Icons.arrow_back, color: Colors.black),
-              onPressed: null),
+          title: const Text('회원정보 찾기', style: TextStyle(color: Colors.black)),
+          leading: const IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: null,
+          ),
           bottom: PreferredSize(
-              child: Container(
-                child: Column(
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: 1.0,
-                      color: colorScheme.onTertiary,
-                    ),
-                    TabBar(
-                      tabs: <Widget>[
-                        Tab(
-                          height: 40,
-                          child: Text(
-                            '아이디 찾기',
-                            style: TextStyle(color: Colors.black),
-                          ),
+              preferredSize: const Size.fromHeight(50),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: double.infinity,
+                    height: 1.0,
+                    color: colorScheme.onTertiary,
+                  ),
+                  const TabBar(
+                    tabs: <Widget>[
+                      Tab(
+                        height: 40,
+                        child: Text(
+                          '아이디 찾기',
+                          style: TextStyle(color: Colors.black),
                         ),
-                        Tab(
-                          child: Text(
-                            '비밀번호 찾기',
-                            style: TextStyle(color: Colors.black),
-                          ),
+                      ),
+                      Tab(
+                        child: Text(
+                          '비밀번호 찾기',
+                          style: TextStyle(color: Colors.black),
                         ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              preferredSize: Size.fromHeight(50)),
+                      ),
+                    ],
+                  ),
+                ],
+              )),
         ),
-        body: SafeArea(
-          child: const TabBarView(
-            children: <Widget>[
-              IdSection(),
-              PwSection(),
-            ],
+        body: const SafeArea(
+          child: TabBarView(
+            children: <Widget>[IdSection(), PwSection()],
           ),
         ),
       ),
@@ -90,23 +83,27 @@ class IdSection extends StatelessWidget {
         width: double.infinity,
         height: double.infinity,
         color: Colors.white,
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('아이디 (이메일) 찾기',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
-            SizedBox(height: 10),
-            Text('아이디 검색을 위해 휴대폰 인증을 진행해 주세요.',
-                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400)),
-            Spacer(),
+            const Text(
+              '아이디 (이메일) 찾기',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              '아이디 검색을 위해 휴대폰 인증을 진행해 주세요.',
+              style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+            ),
+            const Spacer(),
             ElevatedButton(
               onPressed: () {},
-              child: Text('휴대폰 인증하기', style: TextStyle(fontSize: 14)),
               style: ElevatedButton.styleFrom(
                 primary: colorScheme.primary,
-                minimumSize: const Size.fromHeight(50), // NEW
+                minimumSize: const Size.fromHeight(50),
               ),
+              child: const Text('휴대폰 인증하기', style: TextStyle(fontSize: 14)),
             )
           ],
         ));
@@ -119,37 +116,43 @@ class PwSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Colors.white,
-        padding: EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('비밀번호 찾기',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
-            SizedBox(height: 10),
-            Text('회원가입 시 작성하신 아이디를 입력해 주세요.\n본인 확인을 위해 본인 인증을 진행해 주세요.',
-                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400)),
-            SizedBox(height: 40),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(color: colorScheme.primary)),
-                labelText: '이메일 입력',
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.white,
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Text(
+            '비밀번호 찾기',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            '회원가입 시 작성하신 아이디를 입력해 주세요.\n본인 확인을 위해 본인 인증을 진행해 주세요.',
+            style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+          ),
+          const SizedBox(height: 40),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: colorScheme.primary),
               ),
+              labelText: '이메일 입력',
             ),
-            Spacer(),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('다음', style: TextStyle(fontSize: 14)),
-              style: ElevatedButton.styleFrom(
-                primary: colorScheme.primary,
-                minimumSize: const Size.fromHeight(50), // NEW
-              ),
-            )
-          ],
-        ));
+          ),
+          const Spacer(),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              primary: colorScheme.primary,
+              minimumSize: const Size.fromHeight(50), // NEW
+            ),
+            child: const Text('다음', style: TextStyle(fontSize: 14)),
+          )
+        ],
+      ),
+    );
   }
 }
