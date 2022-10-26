ccd687e0c769833626a59500e305a864f8caabe1
andy <wkdgus1164@naver.com>
2022-10-26 21:47:40 +0900

    feat: CustomListTimeWithArrow

---
+++ lib/domain/common/list_tile.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:si_hicoach_fe/domain/common/divider.dart';
+
+class CustomListTileWithArrow extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final VoidCallback? onTap;
+
+  const CustomListTileWithArrow({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          onTap: () => Navigator.pop(context),
+          title: Text(title),
+          subtitle: Text(subtitle),
+          trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+        ),
+        const CustomDivider(),
+      ],
+    );
+  }
+}
--- lib/domain/trainer/study/edit/exercise_add.dart
+++ lib/domain/trainer/study/edit/exercise_add.dart
@@ -1,50 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:si_hicoach_fe/domain/common/app_bar/arrow_back_icon.dart';
-import 'package:si_hicoach_fe/domain/common/color_schemes.dart';
 import 'package:si_hicoach_fe/domain/common/constants.dart';
-import 'package:si_hicoach_fe/domain/trainer/study/edit/exercise_add_item.dart';
+import 'package:si_hicoach_fe/domain/common/divider.dart';
+import 'package:si_hicoach_fe/domain/common/list_tile.dart';
 
 class TrainerStudyEditExerciseAdd extends StatelessWidget {
   const TrainerStudyEditExerciseAdd({Key? key}) : super(key: key);
-
-  _handleSearchButtonPressed() {
-    print('_handleSearchButtonPressed');
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBarArrowBack(titleText: '운동 추가'),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: defaultPadding,
-                  right: defaultPadding,
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: '검색어를 입력해 주세요.',
-                    hintStyle: const TextStyle(color: Colors.grey),
-                    border: InputBorder.none,
-                    suffix: IconButton(
-                      onPressed: _handleSearchButtonPressed,
-                      icon: const Icon(Icons.search),
-                      color: Colors.grey,
-                    ),
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              width: double.infinity,
+              height: 60,
+              child: Padding(
+                padding: const EdgeInsets.only(left: defaultPadding),
+                child: Center(
+                  child: Row(
+                    children: <Widget>[
+                      const Expanded(
+                        flex: 1,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: '검색어를 입력해 주세요.',
+                            hintStyle: TextStyle(color: Colors.grey),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.search_rounded),
+                      ),
+                    ],
                   ),
                 ),
               ),
-              const TrainerStudyEditExerciseAddItem(),
-              const TrainerStudyEditExerciseAddItem(),
-              const TrainerStudyEditExerciseAddItem(),
-              const TrainerStudyEditExerciseAddItem(),
-              const TrainerStudyEditExerciseAddItem(),
-              const TrainerStudyEditExerciseAddItem(),
-            ],
-          ),
+            ),
+            const CustomDivider(),
+            Expanded(
+              flex: 1,
+              child: ListView(
+                children: List.generate(
+                  20,
+                  (int index) => const CustomListTileWithArrow(
+                    title: '벤치프레스',
+                    subtitle: '하체',
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
