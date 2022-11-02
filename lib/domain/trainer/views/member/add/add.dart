8fd2ba833709b21cf81e8bdba87e7f1f6e6059bc
andy <wkdgus1164@naver.com>
2022-11-02 15:03:15 +0900

    refactor: 회원 추가

--- lib/domain/trainer/views/member/add/add.dart
+++ lib/domain/trainer/views/member/add/add.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:si_hicoach_fe/domain/common/components/alert.dart';
 import 'package:si_hicoach_fe/domain/common/components/app_bar.dart';
+import 'package:si_hicoach_fe/domain/common/components/text_field.dart';
 import 'package:si_hicoach_fe/domain/common/components/title_with_description.dart';
 import 'package:si_hicoach_fe/domain/common/constants/constants.dart';
 import 'package:si_hicoach_fe/domain/common/theme/button.dart';
@@ -12,54 +14,59 @@ class TrainerMemberAddView extends StatelessWidget {
   Widget build(BuildContext context) {
     final idController = TextEditingController();
 
-    handleIDValidationButtonClicked() {
-      print('clicked');
+    handleIDValidationButtonClicked(BuildContext context) {
+      showDialog<String>(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) => CustomAlertDialog(
+          title: '검증 완료',
+          content: '등록 가능한 회원입니다.',
+          positiveText: '확인',
+          onPositivePressed: () => Navigator.of(context).pop(),
+        ),
+      );
     }
 
     return Scaffold(
       appBar: const CustomAppBarArrowBack(titleText: '회원 추가'),
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            const TitleWithDescription(
-              title: '회원 아이디 검색',
-              description: '추가하실 회원의 아이디를 검색해 주세요.',
-            ),
-            Container(
-              padding: const EdgeInsets.all(defaultPadding),
-              child: SizedBox(
-                height: 70,
-                child: TextField(
+        child: Container(
+          width: double.infinity,
+          margin: const EdgeInsets.all(defaultPadding),
+          child: Column(
+            children: <Widget>[
+              const TitleWithDescription(
+                title: '회원 아이디 검색',
+                description: '추가하실 회원의 아이디를 검색해 주세요.',
+              ),
+              SizedBox(
+                height: 60,
+                child: CustomTextField(
+                  hintText: '아이디 (이메일) 입력',
                   keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    hintText: '아이디 (이메일) 입력',
-                    labelText: '아이디 (이메일) 입력',
-                    suffix: TextButton(
-                      onPressed: handleIDValidationButtonClicked,
-                      child: const Text('검증'),
-                    ),
+                  suffix: CustomElevatedButton(
+                    onPressed: () => handleIDValidationButtonClicked(context),
+                    text: '검증',
                   ),
                   controller: idController,
                 ),
               ),
-            ),
-            const Spacer(),
-            Container(
-              width: double.infinity,
-              margin: const EdgeInsets.all(defaultPadding),
-              child: CustomElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (BuildContext context) =>
-                            const TrainerMemberAddAdditionalInformation(),
-                      ),
-                    );
-                  },
-                  text: '다음'),
-            ),
-          ],
+              const Spacer(),
+              SizedBox(
+                width: double.infinity,
+                child: CustomElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              const TrainerMemberAddAdditionalInformation(),
+                        ),
+                      );
+                    },
+                    text: '다음'),
+              ),
+            ],
+          ),
         ),
       ),
     );
