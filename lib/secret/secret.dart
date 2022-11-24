ff05bca0ea8a8905deee365dfb08b50758348b18
NightOwl <steelman2019e@gmail.com>
2022-11-24 22:02:30 +0900

    fix: #9

--- lib/secret/secret.dart
+++ lib/secret/secret.dart
@@ -1,3 +1,5 @@
 // ignore_for_file: non_constant_identifier_names
 
 String IAMPORT_USER_CODE = "imp12349201";
+String KAKAO_NATIVE_APP_KEY = "02cfb74cf149bdc54ec9e4e015c7875a";
+String KAKAO_JS_APP_KEY = "4cf0938ebd2039052dc25860e7e73bc6";
