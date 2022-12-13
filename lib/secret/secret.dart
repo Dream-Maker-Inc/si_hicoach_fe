44509e82325af9fec0f16f5c1c21ad82eb49c234
Andy Han <wkdgus1164@naver.com>
2022-12-13 17:22:00 +0900

    feat: iOS kakao link

--- lib/secret/secret.dart
+++ lib/secret/secret.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: non_constant_identifier_names
 
 String IAMPORT_USER_CODE = "imp12349201";
-String KAKAO_NATIVE_APP_KEY = "02cfb74cf149bdc54ec9e4e015c7875a";
-String KAKAO_JS_APP_KEY = "4cf0938ebd2039052dc25860e7e73bc6";
+String KAKAO_NATIVE_APP_KEY = "005ade5c53d77bbb48335bfcb671a7e1";
+String KAKAO_JS_APP_KEY = "affcf5d3ea5d32fc71e35ed0e1a0203e";
